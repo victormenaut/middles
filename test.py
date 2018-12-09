165d378c764ec9b3b74781e93641716c60cf9e5c
victormenaut <victor.e.mena@gmail.com>
2018-12-09 13:57:11 -0600

    file with json and test.py as a converter

--- test.py
+++ test.py
@@ -7,17 +7,13 @@ import json
 def main():
 	nlp = StanfordCoreNLP('/Users/victormena/Development/DPLP/corenlp/', memory='8g')
 	file = open('/Users/victormena/Development/middles/xml_to_text/A/A00.xml.txt', 'r')
-	a = []
 	print('{	', '"sentence"', ': [')
 	for line in file:
 		props={ 'annotators': 'tokenize, ssplit, pos, parse, depparse'}
 		output = nlp.annotate(line, properties=props)
-
-		sem = nlp.semgrex( line, pattern= '{tag: VB}')
-		a.append(sem)
 	print(']}')
 	nlp.close()
-	print(len(a))
+	
 
 
 main()
